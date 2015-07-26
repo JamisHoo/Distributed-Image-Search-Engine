623c3c535bb23d082eaed836fe5b376143201ed0
Jamis <hoojamis@gmail.com>
2015-07-26 23:41:38 +0800

    to be refactor

--- src/main.py
+++ src/main.py
@@ -19,13 +19,49 @@ from __future__ import print_function
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
-import index
-import send_result
+import socket
 
 INDEX_FILE = "../imagenet/index"
+
 QUERY_ADDR = "localhost"
 QUERY_PORT = 20000
 
+RESULT_ADDR = "localhost"                                                       
+RESULT_PORT = 20001
+
+def send_result(rdd_iter):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((RESULT_ADDR, RESULT_PORT))
+
+    try:
+        for record in rdd_iter:
+            sock.send(record)
+    except Exception as e:
+        print("Disconnect")
+        print(e)
+
+    sock.close()
+
+def load_index(index_filename):
+    inverted_index = dict()
+
+    file_handler = open(index_filename)
+    
+    for line in file_handler:
+        keywords = line[: line.find("\t")].split(",")
+
+        positions = []
+        poss = [ int(x, 16) for x in line[line.find("\t") + 1: -1].split(",") ]
+        for i in range(0, len(poss), 3):
+            positions.append((poss[i], poss[i + 1], poss[i + 2]))
+
+        for keyword in keywords:
+            if keyword not in inverted_index:
+                inverted_index[keyword] = positions
+            else:
+                inverted_index[keyword].extend(positions)
+
+    return inverted_index
 
 # Create a StreamingContext and batch interval of 1 second
 sc = SparkContext(appName = "ImageSearch")
@@ -33,7 +69,7 @@ ssc = StreamingContext(sc, 1)
 
 # load inverted index
 # and broadcast to each word node
-inverted_index_var = sc.broadcast(index.load_index(INDEX_FILE))
+inverted_index_var = sc.broadcast(load_index(INDEX_FILE))
 
 # Create a DStream that will connect to hostname:port, like localhost:9999
 # each line is a query, which contains some keywords spliting with spaces
@@ -69,12 +105,13 @@ def search(keywords):
     return list(result)
 
 search_result = keywords.map(search)
-str_result = search_result.map(lambda x: str.encode(str(x)))
+# str_result = search_result.map(lambda x: str.encode(str(x)))
+str_result = search_result.map(lambda x: str.encode(",".join([ format(z, "x") for y in x for z in y ])))
     
 str_result.pprint()
 
 #keywords.pprint()
-str_result.foreachRDD(lambda rdd: rdd.foreachPartition(send_result.send))
+str_result.foreachRDD(lambda rdd: rdd.foreachPartition(send_result))
 
 
 # Start the computation
