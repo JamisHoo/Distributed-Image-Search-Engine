68dae2a53066038e5582d84c2cead7afcdc2ba18
Jamis <hoojamis@gmail.com>
2015-08-02 16:50:53 +0800

    update

--- src/computing_node/search_engine.py
+++ src/computing_node/search_engine.py
@@ -46,6 +46,7 @@ class SearchEngine(BaseHTTPRequestHandler):
 
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
+            self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
             self.wfile.write(result_str.encode("ascii"))
 
